dc0e387c93e7ec86730dca56a3fd36fb3acad726
CamilaChoque <86363351+CamilaChoque@users.noreply.github.com>
2021-08-10 14:36:01 -0300

    Add files via upload

---
+++ Ejercicio_1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+char Letra;
+printf("Introduzca su calificación correspondiente: ");
+Letra=getchar ();
+switch (Letra)
+{
+case 'a':
+case 'A':
+puts ("\nExcelente");
+break;
+
+case 'b':
+case 'B':
+puts ("\nBuena");
+break;
+
+case 'c':
+case 'C':
+puts ("\nRegular");
+break;
+
+case 'd':
+case 'D':
+puts ("\nSuficiente");
+break;
+
+case 'f':
+case 'F':
+puts ("\nNo suficiente");
+break;
+
+default:
+puts ("\nIntentelo de nuevo");
+}
+
+
+ return 0;
+}
